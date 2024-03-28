f7a9f376e14802ebafb941ec23e2815c624b6814
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2024-03-28 14:37:49 +0100

    BorderedRenderer setStroke

--- app/src/main/java/mcr/rendering/BorderedRenderer.java
+++ app/src/main/java/mcr/rendering/BorderedRenderer.java
@@ -8,6 +8,7 @@ public class BorderedRenderer implements Renderer {
     @Override
     public void display(Graphics2D g2d, Bouncable bouncable) {
         g2d.setColor(bouncable.getColor());
+        g2d.setStroke(new BasicStroke(2.0f));
         g2d.draw(bouncable.getShape());
         // TODO: Use the graphics to set the stroke, set the color (with the bouncable color) and
         // draw (with the boucable shape)
